b5331d9e98135397617b22960f05f047e0fb8da3
A Guinane <alexguinane@gmail.com>
2017-12-04 18:36:34 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'nemwriter',
+    packages = ['nemwriter'],
+    version = '0.1.0',
+    description = 'Write meter readings to AEMO NEM12 (interval metering data) and NEM13 (accumulated metering data) data files ',
+    author = 'aguinane',
+    author_email = 'alexguinane@gmail.com',
+    url = 'https://github.com/aguinane/nem-writer',
+    keywords = ['energy', 'NEM12', 'NEM12'],
+    classifiers = [],
+    license = 'MIT',
+)
